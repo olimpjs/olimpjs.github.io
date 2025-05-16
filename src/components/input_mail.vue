495dcae41d5d6c4322a006814b235cda4b05b1f8
olimpjs <140556519+olimpjs@users.noreply.github.com>
2025-05-16 20:47:46 +0000

    очистка

--- src/components/input_mail.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<input type="text" name="input" placeholder="Example@email.com" class="form">
-</template>
-
-<style>
-
-</style>
--- src/components/input_pass.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <input placeholder="Пароль должен содержать 8 символов" form="input" type="text" name="input"class="form">
-    </template>
-    
-    <style>
-    
-    </style>
